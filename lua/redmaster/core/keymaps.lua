6dc2ba88b070f8e28479d0268d02560c09ad9f25
Erasmo Jr <erasmo.cjunior@gmail.com>
2023-10-09 01:37:48 -0300

    :recycle: REFACT: Leader X fecha buffer

--- lua/redmaster/core/keymaps.lua
+++ lua/redmaster/core/keymaps.lua
@@ -16,7 +16,7 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 
 -- tabs
 keymap.set("n", "<leader>bo", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>bx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current t"<cmd>ab
+keymap.set("n", "<leader>x", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current t"<cmd>ab
 keymap.set("n", "<leader>bn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>bp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>bf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
